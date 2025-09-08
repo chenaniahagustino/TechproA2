1a3cf5e73b8464e1d16401987fc053b2b938b7e9
chenaniahagustino <chenaniahagustino.scc@gmail.com>
2025-09-08 11:54:07 +0800

    Update SeatworkFive.java

--- SeatworkFive.java
+++ SeatworkFive.java
@@ -1,14 +1,13 @@
 public class Main {
-    public static void main(String[] args) {
+  public static void main(String[] args) {
+    int countdown = 3;
+    System.out.println("Seatworkfive");
 
-        int countdown = 3;
-        System.out.println("Seatworkfive");
-
-        while (countdown > 0) {
-            System.out.println(countdown);
-            countdown--;
-        }
-
-        System.out.println("Happy Birthday!");
+    while (countdown > 0) {
+      System.out.println(countdown);
+      countdown--;
     }
+    
+    System.out.println("Happy Birthday!!");
+  }
 }
